a9155bf3c428a19766df47fe2cc6738abdc5ec5e
XieAofan <xieaofan2006@outlook.com>
2025-03-05 11:13:59 +0800

    Version: 1.0.0

--- WebSpider.py
+++ WebSpider.py
@@ -1,6 +1,9 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import json
+import time
+from urllib.parse import urlencode
 
 
 class Web:
@@ -8,6 +11,9 @@ class Web:
         self.cookie = None
         self.roomid = None
         self.paperid = None
+        self.versionId = 1
+        self.testid = 1
+        self.loginUserId = 1
         self.headers = {
             "User-Agent": "Mozilla/5.0+(Linux;+Android+10;+ELE-AL00+Build/HUAWEIELE-AL00;+wv)+AppleWebKit/537.36+(KHTML,+like+Gecko)+Version/4.0+Chrome/78.0.3904.62+XWEB/2691+MMWEBSDK/200901+Mobile+Safari/537.36+MMWEBID/215+MicroMessenger/7.0.19.1760(0x2700133F)+Process/toolsmp+WeChat/arm64+NetType/WIFI+Language/zh_CN+ABI/arm64"
         }
@@ -34,11 +40,12 @@ class Web:
             'language': 'zh_CN',
             'webglRenderer': 'ANGLE (Intel, Intel(R) Arc(TM) Graphics (0x00007D55) Direct3D11 vs_5_0 ps_5_0, D3D11)'
         }
-        url = 'http://mapp.nudt.edu.cn/login/login.do'
+        url = 'http://mapp.nudt.edu.cn/login/websubmitapp.do'
         response = requests.post(url, data=post_data, cookies=cookie, headers=headers)
         print(response.status_code)
         if response.status_code == 200:
             self.cookie = cookie
+            print('Login successfully.')
         else:
             raise Exception("Login failed.")
         
@@ -48,7 +55,7 @@ class Web:
         text = response.text
         soup = BeautifulSoup(text, 'html.parser')
         button = soup.find('button', class_='loginbtn')
-        text = button.get('onclick')
+        text2 = button.get('onclick')
         #print(text)
         match = re.search(r"submitRoomPage\('([^']+)','([^']+)'\)", text)
         type = match.groups()[0]
@@ -102,8 +109,170 @@ class Web:
             value = li.input.get('value')
             choices.append((choice, value))
         return ind, q_type, q_img_url, choices
+    
+    def get_review(self):
+        url = 'http://mapp.nudt.edu.cn/websubject/testCardSubjectReview.do'
+        param = {
+            'testid': self.testid,
+        }
+        response = requests.get(url, params=param, cookies=self.cookie, headers=self.headers)
+        if response.status_code != 200:
+            raise Exception("Get review failed.")
+        html = response.text
+        return html
+
+    def get_ans(self,html):
+        soup = BeautifulSoup(html, 'html.parser')
+        big_div = soup.find('div', class_='container wts-paper-forms')
+        raw_divs = list(big_div.children)[7:]
+        divs = []
+        for div in raw_divs:
+            if div == '\n':
+                continue
+            divs.append(div)
+        # '\n                                第1题\xa0 多选题\xa0\n\n\n\n\n\n                            '
+        type_list = ['填空', '单选', '多选', '判断', '问答']
+
+
+        qs = []
+        for div in divs:
+            title = div.find('div', class_='side_unit_info')
+            ind = re.findall(r'第(.*?)题', title.text,re.S)[0]
+            for t in type_list:
+                if t in title.text:
+                    q_type = t
+                    break
+
+            try:
+                q_img_url = div.find('img', style="width: 100%").get('src')
+            except:
+                text = div.find('div', style="text-align: left;").text
+                cut_key = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+                q_img_url = None
+
+            if q_type == '填空':
+                ans = re.findall(r'正确答案：(.*?)\n', div.text, re.S)[0]
+                choice = re.findall(r'for="(.*?)-INPUT">', div.text, re.S)[0]
+                choices = [(ans, choice)]
+                print(ind, q_type, q_img_url)
+                right_ans = [choice]
+            else:
+                anserbox = div.find('div', class_='answerUnitViewBox')
+                ul = anserbox.ul
+                choices = []
+                for li in ul.find_all('li'):
+                    #print(li)
+                    choice = li.label.text.replace(' ', '').replace('\n', '').replace('\xa0', '') # 选项
+
+                    value = li.input.get('value')
+                    choices.append((choice, value))
+                    #print()
+                    #print()
+                a_div = div.find('div', class_='answerRightViewBox')
+                labels = a_div.find_all('label')
+                print(ind, q_type, q_img_url)
+                print(choices)
+                right_ans = []
+                for label in labels:
+                    right_ans.append(label.get('for').replace('-INPUT', ''))
+                    #print(label.get('for').replace('-INPUT', ''))
+                print(right_ans)
+            qs.append((ind, q_type, choices, right_ans))
+        return qs
+    
+    def submit_ans(self, choices, right_ans, qtype):
+        url = 'http://mapp.nudt.edu.cn/websubject/PubRunPoint.do'
+        val = []
+        for i, choice in enumerate(choices):
+            
+            if choice[1] in right_ans:
+                isR = True
+            else:
+                isR = False
+                
+            if qtype == '填空':
+                isR = choice[0]
+
+            d = {
+                'versionid':self.versionId,
+                'answerid':choice[1],
+                'value': isR,
+            }
+            #d = json.dumps(d)
+            val.append(d)
+        #val = json.dumps(val).replace('\\', '')
+        post_data = {
+            'testid': self.testid,
+            'versionId': self.versionId,
+            'val':val,
+            'loginUserId': self.loginUserId,
+            'screenWidth': 390,
+            
+        }
+
+        post_data['val'] = json.dumps(post_data['val']).replace(' ', '')
+        encoded_data = urlencode(post_data)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 9; COL-AL10 Build/HUAWEICOL-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.3527.52 MQQBrowser/6.2 TBS/044607 Mobile Safari/537.36 MMWEBID/7140 MicroMessenger/7.0.4.1420(0x27000437) Process/tools NetType/4G Language/zh_CN',
+            'Accept': '*/*',
+            'Host': 'mapp.nudt.edu.cn',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        try:
+            response = requests.post(url, data=encoded_data, headers=headers, cookies=self.cookie)
+        except:
+            response = requests.post(url, data=encoded_data, headers=headers, cookies=self.cookie)
+        if response.status_code == 200:
+            print(response.text)
+            print('Submit successfully.')
+            return 0
+        
+    def get_result(self, id = 11):
+        url = 'http://mapp.nudt.edu.cn/websubject/PubRandomSubject.do'
+        param = {
+            'index':id,
+            'totalTime': 0,
+            'testid': self.testid,
+            'subjectType': self.subjectType,
+            'userTestUuid': self.loginUserId,
+        }
+        response = requests.get(url, params=param, cookies=self.cookie, headers=self.headers)
+        html = response.text
+        try:
+            self.versionId = re.findall(r'var versionId = "(.*?)";', html)[0]
+        except:
+            print(html)
+        if id == 11:
+            try:
+                jf = re.findall('共获得(.*?)积分', html)[0]
+                print("共获得%"+str(jf)+"积分")
+            except:
+                print(html)
+            return jf
+        else:
+            print('Get VersionID')
+            return True
 
 
 if __name__ == '__main__':
     web = Web()
-    web.login('pctest', '954321')
+    #web.login('ttest', '954321')
+    web.cookie = {'JSESSIONID': '9218E87364A8D217155D740D2C1B93A1'}
+    web.get_roomid()
+    t1 = time.time()
+    web.get_roompage()
+    testid = web.get_testid()
+    html = web.get_review()
+    qs = web.get_ans(html)
+    i = 0
+    for q in qs:
+        i += 1
+        print(i)
+        time.sleep(0.6)
+        web.submit_ans(q[2], q[3], q[1])
+        web.get_result(i+1)
+        #print()
+    t2 = time.time()
+    print(t2-t1)
+    #print(web.get_result())
--- js/home.js
+++ js/home.js
@@ -1,45 +1,332 @@
 
 
-var COOL_DOWN_PERIOD = 60 * 1000; // 默认1分钟冷却期，单位毫秒
 
-function getCooldownPeriod() {
-    var now = new Date();
-    var hour = now.getHours();
+function winNextClick() {
+    window.location.href = 'websubject/PubRandomSubject.do?index=2&timelen=&testid=774d8c09fded481fabdf67f1b80928a0&subjectType=normalType'
+}
 
-   /* if (hour >= 18 && hour < 22) {
-        return 1; // 18时-22时，2分钟冷却期
-    } else if (hour >= 22 || hour <= 9) {
-        return 1; // 22时-早9时，3分钟冷却期
-    } else {
-        return 1; // 其他时间，1分钟冷却期
-    }*/
+window.onload = function () {
+    // 向历史记录栈添加一个新的条目
+    history.pushState(null, null, '/home/index.do');
+    window.addEventListener('popstate', function (event) {
+        // 当用户尝试返回时，再次添加一个新的历史记录条目
+        history.pushState(null, null, '/home/index.do');
+    });
+};
 
-    return 0; // 其他时间，1分钟冷却期
+/*
+    // 在页面加载时绑定事件
+    window.addEventListener('load', function() {
+        // 当用户点击返回键时触发
+        window.history.pushState(null, null, document.URL); // 添加一个状态到历史记录
+        window.addEventListener('popstate', function(event) {
+            // 用户尝试返回时的处理逻辑
+            alert('您正在尝试返回上一页，这里可以插入返回逻辑');
+            // 可以选择再次pushState以防止默认的返回行为
+            window.history.pushState(null, null, document.URL);
+        });
+    });
+    */
+
+
+
+
+var testid = "774d8c09fded481fabdf67f1b80928a0";
+var versionId = "8a9496479490f1ab019490f6ef43005b";
+var subjectId = "8a9496479490f1ab019490f6ef43005a";
+
+
+function submitSubjectVar2() {
+    pAlert("loading...", 10000);
+    $('#submitBtnId').prop('disabled', true); // 禁用按钮
+
+
+    setTimeout(function () {
+        pAlert("网络波动异常，正在重新进入答题...", 10000);
+        window.location.href = '/home/index.do';
+    }, 5000);
+
+    $.post("websubject/PubRunPoint.do", {
+        'testid': testid,
+        'versionId': versionId,
+        'val': enCodePaperForm(),
+        'loginUserId': '3b37e5d6ac7d456cb94cc77b52d140d4',
+        'screenWidth': window.screen.width
+    }, function (flag) {
+        pClose();
+
+        var nexturl = "websubject/PubRandomSubject.do?index=2&totalTime=" + totalTime + "&testid=774d8c09fded481fabdf67f1b80928a0&subjectType=normalType&userTestUuid=3b37e5d6ac7d456cb94cc77b52d140d4";
+        window.location.href = nexturl;
+
+        if (flag.STATE == 0) {
+
+        } else {
+            $('#submitBtnId').prop('disabled', false); // 启用按钮
+            //pAlert(flag.MESSAGE);
+        }
+    }, 'json');
 }
 
 
-function submitRoomPage(subjectType,roomid) {
+// 设置总时间（100分钟）
 
-    if (subjectType == 'betaType') {
-        window.location.href = "exam/roompage.do?roomid="+roomid+"&subjectType=" + subjectType;
-        return;
-    }
+var totalTime = 0;//倒计时多少秒
 
-    const now = new Date().getTime();
-    const lastAnswerTime = parseInt(localStorage.getItem('lastAnswerTime'), 10);
-    var lqsjFz = getCooldownPeriod();
-    const COOL_DOWN_PERIOD = lqsjFz * 60 * 1000; // 获取当前时间段的冷却时间
+function getTotalTime() {
+    var totalTimeStr = '0'.replace(':', '.'); // 去除非数字字符
+    totalTime = Math.floor(totalTimeStr);
+}
 
+function updateCountdown() {
+    if (totalTime <= 0) {
+        pAlert("答题时间已到，将自动提交答案...", 10000);
+        document.getElementById('timelearn').innerHTML = "倒计时结束！";
+        clearInterval(timer); // 停止定时器
 
-    if (lastAnswerTime && now - lastAnswerTime < COOL_DOWN_PERIOD) {
-        // 如果距离上次答题时间不足冷却时间，则显示错误信息
-        pAlert("答题过快，请" + lqsjFz + "分钟后再试...", 3000);
+        setTimeout(function () {
+            var nexturl = "websubject/PubRandomSubject.do?index=11&totalTime=" + totalTime + "&testid=774d8c09fded481fabdf67f1b80928a0&subjectType=normalType";
+            window.location.href = nexturl;
+        }, 1000);
     } else {
-        // 否则，允许答题，并更新最后答题时间
-        localStorage.setItem('lastAnswerTime', now);
-        window.location.href = "exam/roompage.do?roomid="+roomid+"&subjectType=" + subjectType;
+        var minutes = Math.floor(totalTime / 60);
+        var seconds = totalTime % 60;
+        var examTime = padZero(minutes) + ":" + padZero(seconds);
+        document.getElementById('timelearn').innerHTML = examTime;
+        totalTime--; // 每秒减少1秒
     }
 }
 
+// 补零函数，确保两位数显示
+function padZero(num) {
+    return num < 10 ? '0' + num : num;
+}
 
-submitRoomPage('normalType','8a949647953fadb001954615213d5ba7')
+/*
+
+
+
+*/
+
+class Firework {
+    constructor(x, y, targetX, targetY, color) {
+        this.x = x;
+        this.y = y;
+        this.targetX = targetX;
+        this.targetY = targetY;
+        this.color = color;
+        this.particles = [];
+        this.exploded = false;
+    }
+
+    update() {
+        if (!this.exploded) {
+            this.y -= 5;
+            if (Math.abs(this.y - this.targetY) < 5) {
+                this.explode();
+            }
+        } else {
+            for (let particle of this.particles) {
+                particle.update();
+            }
+        }
+    }
+
+    draw(ctx) {
+        if (!this.exploded) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        } else {
+            for (let particle of this.particles) {
+                particle.draw(ctx);
+            }
+        }
+    }
+
+    explode() {
+        this.exploded = true;
+        for (let i = 0; i < 100; i++) {
+            let angle = Math.random() * Math.PI * 2;
+            let speed = Math.random() * 5 + 1;
+            let particleColor = getRandomColor(); // 随机颜色
+            this.particles.push(new Particle(this.x, this.y, angle, speed, particleColor));
+        }
+    }
+}
+
+class Particle {
+    constructor(x, y, angle, speed, color) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.speed = speed;
+        this.color = color;
+        this.life = 100;
+    }
+
+    update() {
+        this.x += Math.cos(this.angle) * this.speed;
+        this.y += Math.sin(this.angle) * this.speed;
+        this.life--;
+    }
+
+    draw(ctx) {
+        if (this.life > 0) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+        }
+    }
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+
+let fireworks = [];
+let canvas;
+let ctx;
+let spawnInterval;
+
+function init() {
+    $("#fireworksCanvas").show();
+    canvas = document.getElementById('fireworksCanvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    window.addEventListener('resize', resizeCanvas);
+    spawnInterval = setInterval(spawnFirework, 1000);
+    // 20秒钟后停止生成烟花
+    let closeTime = 1000 * 20;
+    setTimeout(() => {
+        $("#fireworksCanvas").hide();
+        clearInterval(spawnInterval);
+        stopAnimation();
+        // 再过一段时间后清除所有烟花
+        //animationTimeout = setTimeout(stopAnimation, closeTime);
+    }, closeTime);
+    animate();
+}
+
+function spawnFirework() {
+    let x = Math.random() * canvas.width;
+    let y = canvas.height;
+    let targetX = Math.random() * canvas.width;
+    let targetY = Math.random() * canvas.height * 0.3;
+    let color = getRandomColor(); // 随机颜色
+    fireworks.push(new Firework(x, y, targetX, targetY, color));
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let firework of fireworks) {
+        firework.update();
+        firework.draw(ctx);
+    }
+
+    fireworks = fireworks.filter(firework => !firework.exploded || firework.particles.some(particle => particle.life > 0));
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function stopAnimation() {
+    fireworks = [];
+}
+
+const duration = 10 * 1000; // 20 seconds in milliseconds
+const interval = 500; // Interval between each batch of fireworks in milliseconds
+const fireworksPerBatch = 5; // Number of fireworks per batch
+
+function createFirework() {
+    const firework = document.createElement('div');
+    firework.className = 'firework';
+
+    // Random position
+    var top = Math.random() * window.innerHeight;
+    var left = Math.random() * window.innerWidth;
+
+
+    firework.style.top = top + 'px';
+
+    firework.style.left = left + 'px';
+
+
+    // Random delay
+    const delay = Math.random() * 500;
+    firework.style.animationDelay = delay + 'ms';
+
+    // Append to body
+    document.getElementById('fireworksPer').appendChild(firework);
+
+    // Remove after animation ends
+    firework.addEventListener('animationend', () => {
+        firework.remove();
+    });
+}
+
+function startFireworks() {
+    $("#fireworksPer").attr("style", "display:flex;");
+    const startTime = Date.now();
+    const intervalId = setInterval(() => {
+        if (Date.now() - startTime >= duration) {
+            clearInterval(intervalId);
+            $("#fireworksPer").attr("style", "display:none;");
+        } else {
+            for (let i = 0; i < fireworksPerBatch; i++) {
+                createFirework();
+            }
+        }
+    }, interval);
+}
+
+function jiayou() {
+    $('#jiayouwin').modal('show');
+    setTimeout(function () {
+        $('#jiayouwin').modal('hide');   // 关闭弹窗
+    }, 4000);
+}
+
+// Start the fireworks
+// startFireworks();
+//  jiayou();
+
+//答完题 并且全部题目都做对了 显示烟花效果
+
+
+
+
+function submitSubjectVarPubOwnErr() {
+    pAlert("loading...", 10000);
+    $.post("websubject/PubRunPoint.do", {
+        'testid': testid,
+        'versionId': versionId,
+        'val': enCodePaperForm(),
+        'subjectType': 'pubOwnSubject',
+        'loginUserId': '8a949646955ca1ea01956139cd930c7c'
+    }, function (flag) {
+        pClose();
+        if (flag.STATE == 0) {
+            $('#submitVar-win').modal({
+                keyboard: false
+            })
+            $('#analysisBoxId').hide();
+            $('#rightBoxId').hide();
+            $('#winShowAna').show();
+            $('#winNext').show();
+            $('#myModalLabel').text("答案");
+            if (flag.point == 0) {
+                $('#result-n').show();
+                $('#result-y').hide();
+                $('#result-c').hide();
+       …
